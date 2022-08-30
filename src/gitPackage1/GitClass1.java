7d136d91346ec5bd1fbc5ae17f56dbf369fe42df
ranajitjyoti <ranajitjyoti1@gmail.com>
2022-08-30 12:08:53 +0530

    2nd time push to local repo

--- src/gitPackage1/GitClass1.java
+++ src/gitPackage1/GitClass1.java
@@ -6,7 +6,7 @@ public class GitClass1 {
 		
 		System.out.println("This is Git class 2 ");
 		System.out.println("This is Git class 2ndTime ");
-
+		System.out.println("This is Git class 3ndTime ");
 	}
 
 }
