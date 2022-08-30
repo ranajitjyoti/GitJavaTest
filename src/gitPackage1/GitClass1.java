b404585b9f20d4363639d57ecb08633109423881
ranajitjyoti <ranajitjyoti1@gmail.com>
2022-08-30 11:48:25 +0530

    ranajir 2

--- src/gitPackage1/GitClass1.java
+++ src/gitPackage1/GitClass1.java
@@ -3,7 +3,9 @@ package gitPackage1;
 public class GitClass1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is Git class 2 ");
+		System.out.println("This is Git class 2ndTime ");
 
 	}
 
