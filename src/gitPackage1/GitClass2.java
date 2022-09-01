e3f308ef4b63f241ff07cb5390f16d12349eacf0
Ranajit Jyoti <100518492+ranajitjyoti@users.noreply.github.com>
2022-09-01 12:05:34 +0530

    Update GitClass2.java

--- src/gitPackage1/GitClass2.java
+++ src/gitPackage1/GitClass2.java
@@ -8,6 +8,7 @@ public class GitClass2 {
 		System.out.println("This is Git class 1st Time ");
 		System.out.println("This is Git class 3ndTime ");
 		System.out.println("This is Git class 4th Time ");
+		System.out.println("This is Git class 6th Time on 1st Augoust  2 nd");
 
 	}
 
