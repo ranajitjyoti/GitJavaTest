01e05925041236f56450d969ce2be33e40bb23ba
Ranajit Jyoti <100518492+ranajitjyoti@users.noreply.github.com>
2022-09-01 13:20:59 +0530

    Update GitClass4FromMater.java

--- src/gitPackage1/GitClass4FromMater.java
+++ src/gitPackage1/GitClass4FromMater.java
@@ -5,6 +5,7 @@ public class GitClass4FromMater.java {
 
 	public static void main(String[] args) {
 		System.out.println("This class 4 created from Master ");
+		System.out.println("This class 4 created from Master ");
 
 	}
 
