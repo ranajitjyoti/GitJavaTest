69ff41fa3dc186895adf3540490e04eb05b66557
Ranajit Jyoti <100518492+ranajitjyoti@users.noreply.github.com>
2022-09-01 12:28:53 +0530

    Update GitClass1.java

--- src/gitPackage1/GitClass1.java
+++ src/gitPackage1/GitClass1.java
@@ -11,6 +11,8 @@ public class GitClass1 {
 
 		System.out.println("This is Git class 6th Time on 1st Augoust");
 		System.out.println("class 1This is Git class 5th Time on 31st August ");
+		
+			System.out.println("Code updated in master brnch on 1st August ");
 
 	}
 
